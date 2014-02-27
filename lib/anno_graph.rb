ebb36cfb2fef9c52efaebac48ce6994209eaec96
Lennart Bierkandt <lbierkandt@users.noreply.github.com>
2014-02-27 09:41:00 +0100

    rename program

--- lib/anno_graph.rb
+++ lib/anno_graph.rb
@@ -2,20 +2,20 @@
 
 # Copyright © 2014 Lennart Bierkandt <post@lennartbierkandt.de>
 # 
-# This file is part of GAST.
+# This file is part of GraphAnno.
 # 
-# GAST is free software: you can redistribute it and/or modify
+# GraphAnno is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 # 
-# GAST is distributed in the hope that it will be useful,
+# GraphAnno is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with GAST. If not, see <http://www.gnu.org/licenses/>.
+# along with GraphAnno. If not, see <http://www.gnu.org/licenses/>.
 
 require_relative 'graph'
 require_relative 'search_module'
