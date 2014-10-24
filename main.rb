f7cb9a843152cde900f2aeb332e561c3d5abf2f4
Lennart Bierkandt <lbierkandt@users.noreply.github.com>
2014-10-24 15:59:08 +0200

    use before filter for routes

--- main.rb
+++ main.rb
@@ -32,88 +32,75 @@ controller = GraphController.new
 
 set :root, Dir.pwd
 
-get '/' do
+before do
 	controller.sinatra = self
+end
+
+get '/' do
 	controller.root
 end
 
 get '/graph' do
-	controller.sinatra = self
 	controller.draw_graph
 end
 
 get '/toggle_refs' do
-	controller.sinatra = self
 	controller.toggle_refs
 end
 
 get '/layer_options' do
-	controller.sinatra = self
 	controller.layer_options
 end
 
 post '/commandline' do
-	controller.sinatra = self
 	controller.handle_commandline
 end
 
 post '/sentence' do
-	controller.sinatra = self
 	controller.change_sentence
 end
 
 post '/filter' do
-	controller.sinatra = self
 	controller.filter
 end
 
 post '/search' do
-	controller.sinatra = self
 	controller.search
 end
 
 get '/config' do
-	controller.sinatra = self
 	controller.config_form
 end
 
 post '/config' do
-	controller.sinatra = self
 	controller.save_config
 end
 
 get '/import' do
-	controller.sinatra = self
 	controller.import_form
 end
 
 post '/import' do
-	controller.sinatra = self
 	controller.import_text
 end
 
 get '/new_layer/:i' do
-	controller.sinatra = self
 	controller.new_layer(params[:i])
 end
 
 get '/new_combination/:i' do
-	controller.sinatra = self
 	controller.new_combination(params[:i])
 end
 
 get '/export/subcorpus.json' do
-	controller.sinatra = self
 	controller.export_subcorpus
 end
 
 get '/export_data' do
-	controller.sinatra = self
 	controller.export_data
 end
 
 get '/export/data_table.csv' do
-	controller.sinatra = self
 	controller.export_data_table
 end
 
