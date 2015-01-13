f4259327d3da3d003af5296eea09a6d1b48915d5
Lennart Bierkandt <lbierkandt@users.noreply.github.com>
2015-01-13 20:58:40 +0100

    graph generation: refactoring and better search result highlighting

--- lib/graph_controller.rb
+++ lib/graph_controller.rb
@@ -739,103 +739,94 @@ class GraphController
 		satzinfo[:meta] = build_label(@sentence) if @sentence
 
 		@tokens.each_with_index do |token, i|
-			color = @graph.conf.token_color
-			fontcolor = @graph.conf.token_color
-			if @found && @found[:all_nodes].include?(token)
-				color = @graph.conf.found_color
-				satzinfo[:textline] += '<span class="found_word">' + token.token + '</span> '
-			elsif @filter[:mode] == 'hide' and @filter[:show] != token.fulfil?(@filter[:cond])
-				color = @graph.conf.filtered_color
-				fontcolor = @graph.conf.filtered_color
-				satzinfo[:textline] += '<span class="hidden_word">' + token.token + '</span> '
-			else
-				satzinfo[:textline] += token.token + ' '
-			end
-			token_graph.add_nodes(
-				token.id,
+			options = {
 				:fontname => @graph.conf.font,
 				:label => HTMLEntities.new.encode(build_label(token, @show_refs ? i : nil), :hexadecimal),
 				:shape => 'box',
 				:style => 'bold',
-				:color => color,
-				:fontcolor => fontcolor
-			)
+				:color => @graph.conf.token_color,
+				:fontcolor => @graph.conf.token_color
+			}
+			if @found && @found[:all_nodes].include?(token)
+				options[:color] = @graph.conf.found_color
+				satzinfo[:textline] += '<span class="found_word">' + token.token + '</span> '
+			elsif @filter[:mode] == 'hide' and @filter[:show] != token.fulfil?(@filter[:cond])
+				options[:color] = @graph.conf.filtered_color
+				options[:fontcolor]= @graph.conf.filtered_color
+				satzinfo[:textline] += '<span class="hidden_word">' + token.token + '</span> '
+			else
+				satzinfo[:textline] += token.token + ' '
+			end
+			token_graph.add_nodes(token.id, options)
 		end
 
 		@nodes.each_with_index do |node, i|
-			color = @graph.conf.default_color
+			options = {
+				:fontname => @graph.conf.font,
+				:color => @graph.conf.default_color,
+				:shape => 'box',
+				:label => HTMLEntities.new.encode(build_label(node, @show_refs ? i : nil), :hexadecimal),
+			}
 			add_graphs = []
 			if @filter[:mode] == 'hide' and @filter[:show] != node.fulfil?(@filter[:cond])
-				color = @graph.conf.filtered_color
+				options[:color] = @graph.conf.filtered_color
 			else
 				@graph.conf.layers.each do |l|
 					if node[l.attr] == 't'
-						color = l.color
+						options[:color] = l.color
 						add_graphs << layer_graphs[l.attr]
 					end
 				end
 				@graph.conf.combinations.sort{|a,b| a.attr.length <=> b.attr.length}.each do |c|
 					if c.attr.all?{|a| node[a] == 't'}
-						color = c.color
+						options[:color] = c.color
 						add_graphs << layer_graphs[c.attr]
 					end
 				end
 			end
-			fontcolor = color
+			options[:fontcolor] = options[:color]
 			if @found && @found[:all_nodes].include?(node)
-				color = @graph.conf.found_color
+				options[:color] = @graph.conf.found_color
+				options[:penwidth] = 2
 			end
-			viz_graph.add_nodes(
-				node.id,
-				:fontname => @graph.conf.font,
-				:label => HTMLEntities.new.encode(build_label(node, @show_refs ? i : nil), :hexadecimal),
-				:shape => 'box',
-				:color => color,
-				:fontcolor => fontcolor
-			)
+			viz_graph.add_nodes(node.id, options)
 			add_graphs.each{|g| g.add_nodes(node.id)}
 		end
 
 		@edges.each_with_index do |edge, i|
-			color = @graph.conf.default_color
-			weight = @graph.conf.edge_weight
-			constraint = true
+			options = {
+				:fontname => @graph.conf.font,
+				:label => HTMLEntities.new.encode(build_label(edge, @show_refs ? i : nil), :hexadecimal),
+				:color => @graph.conf.default_color,
+				:weight => @graph.conf.edge_weight,
+				:constraint => true
+			}
 			if @filter[:mode] == 'hide' and @filter[:show] != edge.fulfil?(@filter[:cond])
-				color = @graph.conf.filtered_color
+				options[:color] = @graph.conf.filtered_color
 			else
 				@graph.conf.layers.each do |l|
 					if edge[l.attr] == 't'
-						color = l.color
-						weight = l.weight
-						constraint = false if weight == 0
+						options[:color] = l.color
+						options[:weight]= l.weight
+						options[:constraint] = false if options[:weight] == 0
 					end
 				end
 				@graph.conf.combinations.sort{|a,b| a.attr.length <=> b.attr.length}.each do |c|
 					if c.attr.all?{|a| edge[a] == 't'}
-						color = c.color
-						weight = c.weight
+						options[:color] = c.color
+						options[:weight] = c.weight
 					end
 				end
 			end
-			fontcolor = color
+			options[:fontcolor] = options[:color]
 			if @found && @found[:all_edges].include?(edge)
-				color = @graph.conf.found_color
+				options[:color] = @graph.conf.found_color
+				options[:penwidth] = 2
 			end
-			viz_graph.add_edges(
-				edge.start.id,
-				edge.end.id,
-				:fontname => @graph.conf.font,
-				:label => HTMLEntities.new.encode(build_label(edge, @show_refs ? i : nil),
-				:hexadecimal),
-				:color=> color,
-				:fontcolor => fontcolor,
-				:weight => weight,
-				:constraint => constraint
-			)
+			viz_graph.add_edges(edge.start.id, edge.end.id, options)
 		end
 
 		token_edges.each do |edge|
-			#len => 0
 			viz_graph.add_edges(edge.start.id, edge.end.id, :style => 'invis', :weight => 100)
 		end
 
@@ -861,9 +852,7 @@ class GraphController
 							label += "#{key}: #{value}\n"
 					end
 				end
-				if i
-					label += "t" + i.to_s
-				end
+				label += "t" + i.to_s if i
 			else # normaler Knoten
 				display_attr.each do |key,value|
 					case key
@@ -873,9 +862,7 @@ class GraphController
 							label += "#{key}: #{value}\n"
 					end
 				end
-				if i
-					label += "n" + i.to_s
-				end
+				label += "n" + i.to_s if i
 			end
 		elsif e.kind_of?(Edge)
 			display_attr.each do |key,value|
@@ -886,9 +873,7 @@ class GraphController
 						label += "#{key}: #{value}\n"
 				end
 			end
-			if i
-				label += "e" + i.to_s
-			end
+			label += "e" + i.to_s if i
 		end
 		return label
 	end
