842abfa8070471d3bcee04e527befdbb748bb102
Lennart Bierkandt <lbierkandt@users.noreply.github.com>
2016-08-31 22:58:18 +0200

    edited graph generation error message

--- public/functions.js
+++ public/functions.js
@@ -203,7 +203,7 @@ function updateView(data) {
 	try {
 		var svgElement = new DOMParser().parseFromString(Viz(data['dot'], 'svg'), 'image/svg+xml');
 	} catch(e) {
-		alert('An error occurred while generating the graph. Try the edge label compatibility mode for avoiding this (see config window)');
+		alert('An error occurred while generating the graph. Try reloading your browser window; if that doesn’t help, try the edge label compatibility mode (see config window)');
 		return;
 	}
 	// get new dimensions
