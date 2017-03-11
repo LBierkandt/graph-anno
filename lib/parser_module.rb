122a068fe56332ca18a4f2deb08b237e2a2ea2f1
Lennart Bierkandt <lbierkandt@users.noreply.github.com>
2017-03-11 22:36:40 +0100

    search for i nodes

--- lib/parser_module.rb
+++ lib/parser_module.rb
@@ -234,7 +234,7 @@ module Parser
 				raise "Undefined string \"#{tok[:str]}\""
 			when :bstring
 				p = parse_element(obj[i..-1])
-				if ['in', 'out', 'start', 'end', 'link', 'quant', 'token'].include?(p[:op][:operator])
+				if ['in', 'out', 'start', 'end', 'link', 'quant', 'token', 'i'].include?(p[:op][:operator])
 					terms << p[:op]
 				elsif @conf.layers_and_combinations.map(&:shortcut).include?(tok[:str])
 					terms << {:operator => 'layer', :layers => @conf.layer_by_shortcut[tok[:str]].layers}
