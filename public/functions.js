ed31288a8c468aa8d42b046244997257e329a1e6
Lennart Bierkandt <lbierkandt@users.noreply.github.com>
2017-07-07 09:55:25 +0200

    use viz-lite.js, updated to version 1.8.0

--- public/functions.js
+++ public/functions.js
@@ -195,7 +195,7 @@ function updateView(data) {
 	if (window.originalSvgSize == undefined) originalSvgSize = oldImageSize;
 	// create svg
 	try {
-		var svgElement = new DOMParser().parseFromString(Viz(data.dot, 'svg'), 'image/svg+xml');
+		var svgElement = new DOMParser().parseFromString(Viz(data.dot, {format: 'svg'}), 'image/svg+xml');
 	} catch(e) {
 		alert('An error occurred while generating the graph. Try reloading your browser window or restarting your browser; if that doesn’t help, try the edge label compatibility mode (see config window)');
 		return;
